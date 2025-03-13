5629941a2d5cc2e617bd7efe5fc7a0e92ee6dee1
KWAK RAEHUI <glasofk12345@naver.com>
2025-03-13 18:46:36 +0900

    최종 API 실행행

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-function App(){
+import { useOutlet } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'
+import BsNavBar from "./components/BsNavBar";
+import LoginModal from "./components/LoginModal";
+import { useSelector } from "react-redux";
+
+
+
+function App() {
+    // 현재 라우터에 해당하는 컴포넌트 가져오기
+    const currentOutlet = useOutlet();
+    //로그인 모델의 상태값을 redux store 롭 무터 얻어낸다.
+    const loginModal = useSelector(state=> state.loginModal);
 
     return (
-        <div className="container">
-            <h1>인덱스 페이지</h1>
-        </div>
+        <>
+            <BsNavBar />
+            <div className="container" style={{marginTop:"60px"}}>
+                <div>{currentOutlet}</div>
+            </div>
+            <LoginModal show={loginModal.show}/>
+        </>
+
+
     )
 }
 export default App;
