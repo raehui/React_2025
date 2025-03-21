6154c8bb8f7133efda35d061b0167489c5582054
KWAK RAEHUI <glasofk12345@naver.com>
2025-03-21 11:06:06 +0900

    Quiz 예쁘게 출력, prodressbar 진행중중

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,7 +1,18 @@
 import axios from "axios";
 import { useState } from "react";
-import { Alert, Button, Form } from "react-bootstrap";
+import { Alert, Button, Form, ProgressBar } from "react-bootstrap";
+// gemini 가 응답한 markdown 을 해석하기 위한 패키지 설치 및 import 
 import MarkDown from 'react-markdown';
+// CodeMirror 를 사용하기 위해 3개의 패키지를 설치 하고 import 해야 한다.
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { dracula } from "@uiw/codemirror-theme-dracula";
+// MarkDown 에 코드 블럭을 prettify 하기 위해 
+import rehypeHighlight from "rehype-highlight";
+//import "highlight.js/styles/github.css"; // 깃허브과 동일한 스타일로 코드 디자인이 된다.
+import "highlight.js/styles/atom-one-dark.css" // dark 테마 스타일 코드
+
+
 
 function Quiz() {
     let quizs = ["콘솔창에 1~10 까지 순서대로 출력하는 code를 javascript 로 작성해 보세요",
@@ -9,6 +20,7 @@ function Quiz() {
         "object 에 name 이라는 키값으로 본인의 이름을 넣고 addr 이라는 키값으로 주소를 넣어보세요"
     ];
 
+    
 
     const handleSubmit = () => {
         //질문과 입력한 답을 json 으로 전송한다.
@@ -61,9 +73,11 @@ function Quiz() {
         });
     }
 
+
     return (
         <>
             <h1>javascript 문제</h1>
+            <ProgressBar now={50} label={"50%"} animated/>
             {state.isAnswered ?
                 <div>
                     <h3>체점 결과</h3>
@@ -72,17 +86,22 @@ function Quiz() {
                         :
                         <Alert variant='danger'>오답 입니다.</Alert>
                     }
-                    <MarkDown>{state.comment}</MarkDown>
+                    <MarkDown rehypePlugins={rehypeHighlight}>{state.comment}</MarkDown>
                     {/* <div>{state.comment}</div> */}
                     <Button variant='warning' className='me-3' onClick={handleRetry}> &larr; 다시 풀기</Button>
                     <Button variant='success' onClick={handleNext}>다음문제 &rarr;</Button>
                 </div>
                 :
                 <div>
-                    <Form.Group className='mb-3'>
-                        <Form.Label><strong>{`${state.index+1}.`}</strong> {quizs[state.index]}</Form.Label>
-                        <Form.Control onChange={handleChange} value={state.inputCode} as="textarea" rows="10"></Form.Control>
-                    </Form.Group>
+                    <p>
+                        <strong>{`${state.index+1}.`}</strong> {quizs[state.index]}
+                    </p>
+                    <CodeMirror extensions={[javascript()]}
+                        theme={dracula}
+                        height="300px"
+                        value={state.inputCode} // 코드미러의 초기값 설정
+                        onChange={value => setState({...state, inputCode: value})}
+                    />
                     <Button onClick={handleSubmit}>제출</Button>
                 </div>
             }
